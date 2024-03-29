f0235b4c2991019bf36fcaf0ac353f3fa49b1e67
Troy <tydamon@hotmail.com>
2024-03-29 22:39:02 +1100

    put update routes back in

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -101,6 +101,46 @@ router.get('/comments', async (req, res) => {
   }
 });
 
+/////////////////////////////////////////////////////////////////
+// [one]
+// ============== get posts/projects by id to update ============= //
+
+// this creates the /update URL page
+
+router.get('/update/:id', async (req, res) => {
+  try {
+    const projectData = await Project.findOne({
+      where: { id: req.params.id },
+      attributes: ['id', 'name', 'description', 'date_created'],
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+        {
+          model: Comment,
+          attributes: ['id', 'text', 'project_id', 'user_id', 'date_created'],
+          include: {
+            model: User,
+            attributes: ['name'],
+          }
+        },
+      ],
+    });
+
+    const project = projectData.get({ plain: true });
+
+    res.render('update', {
+      ...project,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+/////////////////////////////////////////////////////////////////////
+
 
 // =========================== Logging in ====================== //
 
@@ -125,6 +165,8 @@ router.get('/profile', withAuth, async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+/////////////////////////////////////////////////////////////////////
 
 // ===== login page ==== //
 
