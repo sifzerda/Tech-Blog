70246f2b204b37e6831e7fd7c18b704644b33f70
Troy <tydamon@hotmail.com>
2024-03-30 15:52:55 +1100

    fixed minor code issue with post

--- public/js/profile.js
+++ public/js/profile.js
@@ -7,7 +7,7 @@ const newFormHandler = async (event) => {
   // fetch user's projects/posts
 
   if (name && description) {
-    const response = await fetch(`/api/projects/update`, {
+    const response = await fetch(`/api/projects`, {
       method: 'POST',
       body: JSON.stringify({ name, description }),
       headers: {
